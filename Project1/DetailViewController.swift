b4ee5a8fc23033b826bef76cf7b13d7fab9db5e6
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-01-11 02:33:12 +0300

    Final commit

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -14,7 +14,6 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
         if let imageToLoad = selectedImage {
             imageView.image = UIImage (named: imageToLoad)
